0bdf89cd815a7e29f77d7f2c6668691384d83809
Neal Howard <nhoward72@yahoo.com>
2020-02-17 17:02:25 -0600

    Does not compile.  still buggy

--- lesson2/FileManager.cs
+++ lesson2/FileManager.cs
@@ -1,18 +1,45 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
 
-public class FileManager
+namespace lesson2
 {
-	public FileManager(List<Tickets> Ticket)
-	{
-        this.Ticket = Ticket;
-
-    }
-    public Save(List<Tickets> Ticket)
+    public class FileManager
     {
-        for (int i = 0; i < Ticket.count; i++)
+        public FileManager(List<Tickets> Ticket)
         {
+            this.Ticket = Ticket;
 
         }
+        public Save(List<Tickets> Ticket)
+        {
+            if (!File.Exists(file))
+            {
+                using (StreamWriter ticketFile = File.CreateText(file))
+                {
+                    for (int i = 0; i < Ticket.Count; i++)
+                    {
+                        ticketFile.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}", Ticket.ticketNumber, Ticket.summary,
+                               Ticket.status, Ticket.priority, Ticket.submittedBy, Ticket.assignedTo, Ticket.watching);
+                        Console.WriteLine("A Ticket has been created for you.");
+                        ticketFile.Close();
+                    }
+                }
+            }
+            else
+            {
 
-
+                StreamWriter ticketFile = new StreamWriter(file);
+                {
+                    for (int i = 0; i < Ticket.Count; i++)
+                    {
+                        ticketFile.WriteLine("{0}|{1}|{2}|{3}|{4}|{5}|{6}", Ticket.ticketNumber, Ticket.summary,
+                               Ticket.status, Ticket.priority, Ticket.submittedBy, Ticket.assignedTo, Ticket.watching);
+                        Console.WriteLine("A Ticket has been created for you.");
+                        ticketFile.Close();
+                    }
+                }
+            }
+        }
     }
+}
